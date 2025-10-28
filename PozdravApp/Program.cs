1a5fc0814e0bd8c431bb46bda41124aea30f5b53
Dominik Peric <peric.dominik7@gmail.com>
2025-10-28 12:46:52 +0100

    Initial console app, ispis pozdrava

--- PozdravApp/Program.cs
+++ PozdravApp/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Pozdrav iz aplikacije od Dominik Peric, broj indeksa: 0165091756");
+
+Console.WriteLine("Pozdrav iz aplikacije od Dominik Peric");
+
